956ff9ab97e94cc43b6a1107cf7233e7574104c7
Ruben Hamelink <ruben.hamelink@hotmail.com>
2020-06-19 18:22:46 +0200

    Updated orderservice to call the actual customerservice

--- Microservices/OrderService/OrderService/Services/CustomerService.cs
+++ Microservices/OrderService/OrderService/Services/CustomerService.cs
@@ -1,13 +1,29 @@
 ﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OrderService.Services
 {
     public class CustomerService : ICustomerService
     {
-        // TODO call to actual service
+        private IConfiguration configuration;
+
+        public CustomerService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        
         public bool Exists(string id)
         {
-            return true;
+            string address = configuration["CustomerServiceAddress"];
+            using HttpClient client = new HttpClient();
+            string url = $"http://{address}/Customer?id={id}";
+            var customerStringTask = client.GetStringAsync(url);
+            customerStringTask.Wait();
+            var customerString = customerStringTask.Result;
+            return !string.IsNullOrEmpty(customerString);
         }
     }
 }
