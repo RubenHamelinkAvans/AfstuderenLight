78dbe2b0c1347d89b8a4eb20a390c727de4d9e63
Ruben Hamelink <ruben.hamelink@hotmail.com>
2020-06-21 13:19:35 +0200

    Docker compose contains monolithic app and added logging to customerservice

--- Microservices/CustomerService/src/main/java/com/avans/customerservice/controllers/CustomerController.java
+++ Microservices/CustomerService/src/main/java/com/avans/customerservice/controllers/CustomerController.java
@@ -2,6 +2,8 @@ package com.avans.customerservice.controllers;
 
 import com.avans.customerservice.domain.Customer;
 import com.avans.customerservice.services.CustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Customer")
+@Slf4j
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -21,11 +24,13 @@ public class CustomerController {
 
     @GetMapping
     public Customer get(String id){
+        log.info("Getting customer information for " + id);
         return customerService.getCustomer(id);
     }
 
     @PostMapping
     public String create(Customer customer){
+        log.info("Creating customer " + customer);
         return customerService.createCustomer(customer);
     }
 
